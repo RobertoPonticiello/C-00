2144c268912e0e9b3de1c09750379fb6bfcb1475
Roberto nicola Ponticiello <rpontici@e3r3p8.42roma.it>
2025-12-16 21:03:44 +0100

    Quasi finito il secondo es

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,4 +1,53 @@
+void PhoneBook::addContact()
+{
+    std::string input;
+
+    if (nextIndex > 7)
+    {
+        nextIndex = 0;
+    }
+    std::cout << "Inserisci il nome: " << std::endl;
+    std::getline(std::cin, input);
+    contacts[nextIndex].setFirstName(input);
+    std::cout << "Inserisci il cognome: " << std::endl;
+    std::getline(std::cin, input);
+    contacts[nextIndex].setLastName(input);
+    std::cout << "Inserisci il Nickname: " << '\n';
+    std::getline(std::cin, input);
+    contacts[nextIndex].setNickname(input);
+    std::cout << "Inserisci il Numero di telefono: " << '\n';
+    std::getline(std::cin, input);
+    contacts[nextIndex].setNumber(input);
+    std::cout << "Inserisci il Segreto: " << '\n';
+    std::getline(std::cin, input);
+    contacts[nextIndex].setSecret(input);
+}
+
+void PhoneBook::search()
+{
+    std::string input;
+
+    void printTable() const;
+    std::cout << "Inserisci un indice" << '\n';
+    std::getline(std::cin, input);
+    contacts[nextIndex].setFirstName(input);
+    void printTable(input) const;
+}
+
+void PhoneBook::printTable()
+{
+    std::string input;
+
+    std::cout << "Inserisci un indice" << '\n';
+    std::getline(std::cin, input);
+    void printContact(int index) const;
+}
+
 int main(int argc, char **argv)
 {
-    
+    std::string input;
+
+    std::cout << "IL PROGRAMMA PRENDE SOLO STE 3 COSE, SCE'" << '\n';
+    std::cout << "ADD  -  SEARCH  -  EXIT" << '\n';
+
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -2,6 +2,31 @@
 #define PHONEBOOK_HPP
 
 #include <string>
+#include "Contact.hpp"
+
+class Contact {
+private:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string number;
+    std::string darkest_secret;
+
+public:
+    Contact();
+
+    void setFirstName(std::string first_name);
+    void setLastName(std::string last_name);
+    void setNickname(std::string nickname);
+    void setNumber(std::string number);
+    void setSecret(std::string darkest_secret);
+
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getNickname() const;
+    std::string getNumber() const;
+    std::string getSecret() const;
+};
 
 class PhoneBook {
 private:
@@ -18,31 +43,5 @@ private:
     void printTable() const;
     void printContact(int index) const;
 };
-
-
-class Contact {
-private:
-    std::string first_name;
-    std::string last_name;
-    std::string nickname;
-    std::string number;
-    std::string darkest_secret;
-
-public:
-    Contact();    // costruttore
-
-    void setFirstName(std::string first_name);
-    void setLastName(std::string last_name);
-    void setNickname(std::string nickname);
-    void setNumber(std::string number);
-    void setSecret(std::string darkest_secret);
-
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getNickname() const;
-    std::string getNumber() const;
-    std::string getSecret() const;
-};
-
 
 #endif
